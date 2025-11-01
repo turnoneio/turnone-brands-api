651d3c766d5ad40e207e4bf0c133d45e3c69a62a
turnoneio <inquiries@turnone.io>
2025-10-31 23:05:25 -0700

    Update brands.js

--- api/brands.js
+++ api/brands.js
@@ -1,5 +1,9 @@
-import brands from '../brands.json' assert { type: 'json' };
+import fs from 'fs';
+import path from 'path';
 
 export default function handler(req, res) {
+  const filePath = path.join(process.cwd(), 'brands.json');
+  const data = fs.readFileSync(filePath, 'utf8');
+  const brands = JSON.parse(data);
   res.status(200).json(brands);
 }
